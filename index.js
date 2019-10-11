5036d42c13692cbf9e8f542219e1edf1e916a56f
Connor Turland <connorturland@gmail.com>
2019-10-11 12:15:38 -0400

    enable mongo storing of chat_ids

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const EventEmitter = require('events') // nodejs native import
 const TelegramBot = require('node-telegram-bot-api')
+const MongoClient = require('mongodb').MongoClient
 
 // this key will be also used by other classes that implement the "Contactable"
 // trait/contract
@@ -11,29 +12,55 @@ module.exports.STANDARD_EVENT_KEY = STANDARD_EVENT_KEY
 const TYPE_KEY = 'telegram'
 module.exports.TYPE_KEY = TYPE_KEY
 
+const MONGO_COLLECTION = 'telegram_chat_ids'
+
 let telegramBot
 // map from telegram usernames to chat ids with the bot
 // (hint: the chat id seems to be the same as the user id, interestingly)
 let usernameChatIdMap = {}
-function setUsernameChatIdFromMessage(msg) {
+async function setUsernameChatIdFromMessage(msg) {
     const { username, id } = msg.chat
     console.log('setting telegram chat_id for username ' + username + ': ' + id)
     usernameChatIdMap[username] = id
+    if (mongoClient) {
+        const check = await getChatIdFromUsername(username)
+        if (!check) {
+            mongoClient.collection(MONGO_COLLECTION).insertOne({
+                chat_id: id,
+                username
+            })
+        }
+    }
+}
+async function getChatIdFromUsername(username) {
+    if (mongoClient) {
+        const record = await mongoClient.collection(MONGO_COLLECTION).findOne({ username })
+        return record ? record.chat_id : null
+    } else {
+        return Promise.resolve(usernameChatIdMap[username])
+    }
 }
 
-let eventBus
+let eventBus, mongoConnect, mongoClient
 
 // telegram 'updates' seem to be transient
 // ... if telegram thinks you have received that update
 // they will no longer return it in the getUpdates results
-module.exports.init = async (token) => {
+module.exports.init = async (token, mongoUri, dbName) => {
     console.log('initializing rsf-telegramable')
     telegramBot = new TelegramBot(token, { polling: true })
 
     // a singleton that will act to transmit events between the webhook listener
     // and the instances of Textable
     eventBus = new EventEmitter()
+
+    // intentionally don't catch error
+    if (mongoUri) {
+        mongoConnect = await MongoClient.connect(mongoUri)
+        mongoClient = mongoConnect.db(dbName)
+    }
     
+    // process any backlog and save it
     try {
         const updates = await telegramBot.getUpdates()
         updates.forEach(update => {
@@ -60,6 +87,11 @@ const shutdown = async () => {
     eventBus.removeAllListeners()
     telegramBot = null
     eventBus = null
+    if (mongoConnect) {
+        mongoConnect.close()
+        mongoConnect = null
+        mongoClient = null
+    }
 }
 module.exports.shutdown = shutdown
 
@@ -86,10 +118,11 @@ class Telegramable extends EventEmitter {
     // expose a function that conforms to the standard for Contactable
     // which can "reach" the person
     async speak(string) {
-        const chatId = usernameChatIdMap[this.id]
+        const chatId = await getChatIdFromUsername(this.id)
         if (chatId) {
             telegramBot.sendMessage(chatId, string)
         } else {
+            // TODO, queue this up?
             console.log('Tried to send message to ' + this.id + ' but did not have chat_id')
         }
     }
